62ce7c82f6e3fb81393231bdd52df02910dec058
php-ini <jinkazama_m@yahoo.com>
2016-05-14 16:14:23 -0400

    first commit late

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -11,6 +11,9 @@
 |
 */
 
-Route::get('/', function () {
+/*Route::get('/', function () {
     return view('welcome');
 });
+*/
+
+Route::get('/', ['as' => 'homePage', 'uses' => 'productController@home']);
